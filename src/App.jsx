2cca49838e0150eecd3b345181115fb87adaed24
Axocon <78494636+HoRonTy@users.noreply.github.com>
2026-03-04 12:32:28 +0000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      margin: 0,
+      fontFamily: 'Arial, sans-serif',
+      fontSize: '3rem',
+      color: '#333'
+    }}>
+      Привет!
+    </div>
+  )
+}
+
+export default App
